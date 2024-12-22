29dd9422bfe76657e603b0839c9ac8123660d064
HYEONGEUNKIM <khg1405@gmail.com>
2024-12-23 00:52:00 +0900

    test

--- routes/test.js
+++ routes/test.js
@@ -13,27 +13,70 @@ const axios_get = (options) => {
       .get(options.uri, options)
       .then((response) => {
         if (response.data) {
+          console.log(response);
+
           var jsonData = { msg: "good" };
-          resolve(jsonData);
+          resolve();
         } else {
           resolve();
         }
       })
       .catch((err) => {
-        console.error(err);
         resolve({});
       });
   });
 };
 
+const request_post = (options) => {
+  return new Promise(async (resolve) => {
+    request.get(options, async (err, response, body) => {
+      if (err) throw err;
+
+      const buffer = Buffer.from(body, "utf-8"); // Base64 디코딩
+
+      var data = buffer.toString("Base64");
+      // Base64 디코딩
+      const buffer_data = Buffer.from(data, "base64");
+      console.log();
+
+      resolve(body);
+    });
+  });
+};
+
 const run = async () => {
+  var currentTimestamp = Date.now();
+  var randomNumber = 0;
+  if (currentTimestamp % 2 == 0) {
+    randomNumber = BigInt(Math.floor(Math.random() * 9e22) + 1e22);
+    randomNumber = randomNumber.toString();
+  } else {
+    randomNumber = BigInt(Math.floor(Math.random() * 9e21) + 1e21);
+    randomNumber = randomNumber.toString();
+  }
+  var cookie_num = randomNumber;
+  var jar = request.jar();
+  var cookie_string = `MARKETID=${cookie_num}; PCID=${cookie_num}`;
+  var cookie = tough.Cookie.parse(cookie_string);
+  jar.setCookie(cookie.toString(), "https://m.coupang.com"); // 특정 도메인에 쿠키 설정
   var option = {
-    uri: `https://www.naver.com`,
+    uri: `https://www.coupang.com/vm/products/6393352093/brand-sdp/reviews/list?page=1&slotSize=200&reviewOnly=true&_=1734504688904`,
     method: "get",
     responseType: "arraybuffer",
+    headers: {
+      "User-Agent": "PostmanRuntime/7.43.0",
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+      "Content-Type": "application/json;charset=UTF-8",
+      Accept: "*/*",
+      jar,
+      "Content-Encoding": "gzip",
+      Cookie: `MARKETID=${cookie_num}; PCID=${cookie_num}`,
+    },
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity,
   };
   var data = await axios_get(option);
-  console.log(data);
 
   return data;
 };
